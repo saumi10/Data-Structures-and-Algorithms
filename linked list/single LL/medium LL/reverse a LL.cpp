ca82c197cb74c0fd645e6d87b4d49393f45de7f5
saumi10 <saumi2310@gmail.com>
2025-05-24 20:47:36 +0530

    day13

--- linked list/single LL/medium LL/reverse a LL.cpp
+++ linked list/single LL/medium LL/reverse a LL.cpp
@@ -10,12 +10,9 @@ struct ListNode {
 };
 
 ListNode* reverseList(ListNode* head) {
-        ListNode* temp=head->next;
-        ListNode* prev=head;
-        if(head==NULL || head->next==NULL){
-            return head;
-        }
-        prev->next=NULL;
+        
+        ListNode* temp=head;
+        ListNode* prev=nullptr;
         while(temp!=NULL){
             //storing before changing link
             ListNode* front=temp->next;
@@ -28,7 +25,7 @@ ListNode* reverseList(ListNode* head) {
 }
 
 int main(){
-    vector<int> v={1,2,3,4,5,6,7,53,38687,73,88,99,10};
+    vector<int> v={1,2,3,4,5,6,7,53,38687,73,88,99,10,12};
     ListNode * head=new ListNode (v[0]); // Create the head ListNode
     ListNode * mover=head; // Pointer to track the last ListNode
     for(int i=1;i<v.size();i++){
