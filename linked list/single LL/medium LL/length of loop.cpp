2ca8f69a2ed8d73649eba4f426e5fb0fa286169e
saumi10 <saumi2310@gmail.com>
2025-05-25 01:55:31 +0530

    day14

---
+++ linked list/single LL/medium LL/length of loop.cpp
@@ -0,0 +1,5 @@
+//approach 1: Hashing
+
+//aproach 2: detect loop using slow and fast pointers, and then call a fuction to determine length  of loop,
+//from here , send one of the two pointers to traverse the loop and when they meet again u can keep track using count
+//that will be ur length of loop
---
+++ linked list/single LL/medium LL/starting point of loop.cpp
@@ -0,0 +1,18 @@
+ //S.C =O(1) T.C=O(N)
+    //     ListNode *slow=head;
+    //     ListNode *fast=head;
+    //     while(fast!=NULL && fast->next!=NULL){
+    //         slow=slow->next;
+    //         fast=fast->next->next;
+    //         if(slow==fast){
+    //             slow=head;
+    //             while(slow!=fast){
+    //                 slow=slow->next;
+    //                 fast=fast->next;
+                    
+    //             }
+    //             return slow;
+    //         }
+    //     }
+    //     return NULL;
+    
