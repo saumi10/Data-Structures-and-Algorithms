37a934ee16fc685777533e2dcec3302929d0cf18
saumi10 <saumi2310@gmail.com>
2025-05-28 19:54:19 +0530

    day16

---
+++ contest problems to review/may28/ques2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string solve(string s){
+
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+
+        string s;
+        cin>>s;
+
+        string ans=solve(s);
+
+        cout<<ans;
+    }
+}
